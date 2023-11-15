abab4bcdc6fc73ddb262bfb22b913dbcd297214d
PotticusMaximus <136069407+PotticusMaximus@users.noreply.github.com>
2023-11-15 10:37:08 +0000

    icons now update and alt text does too

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import WeatherCard from "./components/WeatherCard";
+import Location from "./components/Location";
 import Data from "./data";
-// Import data and WeatherCard here
+import { useState } from "react";
 
 function App() {
+  const [location, setLocation] = useState("London");
+
   return (
     <>
       <h1 className="title">REACTIVE WEATHER</h1>
       <h3 className="subtitle">Up to the minute weather news</h3>
       <div className="app">
         {Data.map((city) => (
-          <WeatherCard city={city} />
+          <WeatherCard
+            city={city} //updateImg={updateImg}
+          />
         ))}
       </div>
     </>
--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -1,21 +1,41 @@
 import React from "react";
 import sunny from "../assets/Sunny.svg";
+import rainy from "../assets/Rainy.svg";
+import cloudy from "../assets/Cloudy.svg";
+import partlyCloudy from "../assets/PartlyCloudy.svg";
+import { useState } from "react";
 
-function WeatherCard(props) {
+function WeatherCard({ city }) {
+  //
+  function updateImg(forecast) {
+    switch (forecast) {
+      case "Sunny":
+        return sunny;
+      case "Rainy":
+        return rainy;
+      case "Cloudy":
+        return cloudy;
+      case "Partly cloudy":
+        return partlyCloudy;
+      default:
+        return sunny;
+    }
+  }
+
   return (
     <div className="card">
       <div className="img-container">
         <img
           className="card-img-top"
-          src={sunny}
-          alt="Card image cap"
+          src={updateImg(city.forecast)}
+          alt={city.forecast}
           id="icon"
         />
       </div>
       <div class="card-body">
-        <h3 className="card-title">{props.city.city}</h3>
-        <h5 className="card-text">{props.city.temperature}</h5>
-        <h5 className="card-text">{props.city.forecast}</h5>
+        <h3 className="card-title">{city.city}</h3>
+        <h5 className="card-text">{city.temperature}</h5>
+        <h5 className="card-text">{city.forecast}</h5>
       </div>
     </div>
   );
